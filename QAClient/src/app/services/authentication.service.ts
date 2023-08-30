7c2c450cae174ba25cebd351034f218297fe943b
Lori <lorikmanaj@hotmail.com>
2023-08-30 15:36:20 +0200

    Added Auth service angular

---
+++ QAClient/src/app/services/authentication.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Login } from '../models/login';
+import { Register } from '../models/register';
+import { JwtAuth } from '../models/jwtAuth';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+  registerUrl = "Auth/Register";
+  loginUrl = "Auth/Login";
+  weatherUrl = "WeatherForecast";
+
+  constructor(private http: HttpClient) { }
+
+  public register(user: Register): Observable<JwtAuth> {
+    return this.http.post<JwtAuth>(`${environment.apiUrl}/${this.registerUrl}`, user);
+  }
+
+  public login(user: Login): Observable<JwtAuth> {
+    return this.http.post<JwtAuth>(`${environment.apiUrl}/${this.loginUrl}`, user);
+    }`, user);
+  }
+}
--- QAClient/src/environments/environment.ts
+++ QAClient/src/environments/environment.ts
@@ -3,6 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  apiUrl: "http://localhost:5020/api",
   production: false
 };
 
