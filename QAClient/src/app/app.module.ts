fadc89e240285a75cbf4eed622bef4378e77bcca
Lori <lorikmanaj@hotmail.com>
2023-09-14 16:01:40 +0200

    Added initial components on ClientApp

--- QAClient/src/app/app.module.ts
+++ QAClient/src/app/app.module.ts
@@ -8,10 +8,40 @@ import { AppComponent } from './app.component';
 
 import { FormsModule } from '@angular/forms';
 import { AuthenticationInterceptor } from './services/interceptor';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MainsectionComponent } from './components/mainsection/mainsection.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { HeadlineComponent } from './components/headline/headline.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductListingComponent } from './components/product-listing/product-listing.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { AdminPanelComponent } from './components/admin-panel/admin-panel.component';
+import { UserOrdersComponent } from './components/user-orders/user-orders.component';
+import { AdminOrdersComponent } from './components/admin-orders/admin-orders.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProductFiltersComponent } from './components/product-filters/product-filters.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    MainsectionComponent,
+    CategoriesComponent,
+    HeadlineComponent,
+    ProductComponent,
+    ProductListingComponent,
+    FooterComponent,
+    UserProfileComponent,
+    AdminPanelComponent,
+    UserOrdersComponent,
+    AdminOrdersComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProductFiltersComponent
   ],
   imports: [
     BrowserModule,
