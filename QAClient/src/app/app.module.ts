d0cbcb122bad6111f68c3ed9679b14adc163fd5b
Lori <lorikmanaj@hotmail.com>
2023-09-20 12:47:45 +0200

    Fixed dependencies

--- QAClient/src/app/app.module.ts
+++ QAClient/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-//import { CarouselModule } from 'ngx-owl-carousel-o';
 
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
