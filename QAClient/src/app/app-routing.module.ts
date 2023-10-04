ea174c4b6f5168848142fc3b53c9f80f594c77d5
vuetim <vuetimvue@gmail.com>
2023-10-04 03:48:41 -0700

    Added ProductDetailsComponent, chenged style of Product Title

--- QAClient/src/app/app-routing.module.ts
+++ QAClient/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListingComponent } from './components/product-listing/product-listing.component';
 import { HomeComponent } from './components/home/home.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component'
 
 const routes: Routes = [
   { path: 'products/home', component: HomeComponent },
   { path: 'products/:type', component: ProductListingComponent },
+  { path: 'product/:id', component: ProductDetailsComponent },
   { path: '', redirectTo: '/products/home', pathMatch: 'full' }, // Default route
   { path: '**', redirectTo: '/' }, // Handle invalid routes
 ];
--- QAClient/src/app/components/product-details/product-details.component.ts
+++ QAClient/src/app/components/product-details/product-details.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  /* Slider Images  */
+  imgId = 1;
+
+  ngOnInit() {
+    this.initializeImageSlider();
+  }
+
+  initializeImageSlider() {
+    const imgs = document.querySelectorAll('.img-select a');
+    const imgBtns = Array.from(imgs);
+
+
+
+    imgBtns.forEach((imgItem) => {
+      imgItem.addEventListener('click', (event: Event) => {
+        event.preventDefault();
+        this.imgId = parseInt(imgItem.getAttribute('data-id') || '1', 10);
+        this.slideImage();
+    });
+
+    });
+
+    window.addEventListener('resize', () => this.slideImage());
+  }
+
+  slideImage() {
+    const displayWidth = (document.querySelector('.img-showcase img:first-child') as HTMLElement)?.clientWidth || 0;
+    const imgShowcase = document.querySelector('.img-showcase') as HTMLElement;
+    if (imgShowcase) {
+      imgShowcase.style.transform = `translateX(${- (this.imgId - 1) * displayWidth}px)`;
+    }
+  }
+  /* Slider Images fund */
 
 }
