d9233efbb4e015a7018925fa2a83d00c7166496a
Lori <lorikmanaj@hotmail.com>
2023-10-11 12:13:32 +0200

    Updated DB (Tags & Order Status)

---
+++ QafenAkullAPI/QafenAkullAPI/Infrastructure/Migrations/20231011101305_Added Prod Tags and Ord Status.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace QafenAkullAPI.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedProdTagsandOrdStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: "78ec4f78-f7ed-49e5-b05d-fa16619fe158");
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: "bbc6861c-9cdc-4ee5-9fe5-7733cb110da7");
+
+            migrationBuilder.DropColumn(
+                name: "Approved",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrderStatusId",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "OrderStatuses",
+                columns: table => new
+                {
+                    StatusId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderStatuses", x => x.StatusId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductTags",
+                columns: table => new
+                {
+                    TagId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    Tag = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductTags", x => x.TagId);
+                    table.ForeignKey(
+                        name: "FK_ProductTags_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "238024b3-6db6-487d-b498-24b4d74272ff", null, "Administrator", "ADMINISTRATOR" },
+                    { "2b731802-ddd1-47f4-95d7-10e30b95c79a", null, "User", "USER" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_OrderStatusId",
+                table: "Orders",
+                column: "OrderStatusId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductTags_ProductId",
+                table: "ProductTags",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_OrderStatuses_OrderStatusId",
+                table: "Orders",
+                column: "OrderStatusId",
+                principalTable: "OrderStatuses",
+                principalColumn: "StatusId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_OrderStatuses_OrderStatusId",
+                table: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "OrderStatuses");
+
+            migrationBuilder.DropTable(
+                name: "ProductTags");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_OrderStatusId",
+                table: "Orders");
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: "238024b3-6db6-487d-b498-24b4d74272ff");
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: "2b731802-ddd1-47f4-95d7-10e30b95c79a");
+
+            migrationBuilder.DropColumn(
+                name: "OrderStatusId",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Approved",
+                table: "Orders",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "78ec4f78-f7ed-49e5-b05d-fa16619fe158", null, "Administrator", "ADMINISTRATOR" },
+                    { "bbc6861c-9cdc-4ee5-9fe5-7733cb110da7", null, "User", "USER" }
+                });
+        }
+    }
+}
