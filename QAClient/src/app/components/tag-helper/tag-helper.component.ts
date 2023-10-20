4d9ed06a69b7e978c81e3f534b5d49d1c305b06c
Lori <lorikmanaj@hotmail.com>
2023-10-20 23:33:25 +0200

    Fixed tag service

--- QAClient/src/app/components/tag-helper/tag-helper.component.ts
+++ QAClient/src/app/components/tag-helper/tag-helper.component.ts
@@ -1,39 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductTag } from 'src/app/models/productTag';
+import { TagsService } from 'src/app/services/tags.service';
 
 @Component({
   selector: 'app-tag-helper',
   templateUrl: './tag-helper.component.html',
   styleUrls: ['./tag-helper.component.css']
 })
-export class TagHelperComponent {
-  tags: ProductTag[] = [
-    { tagId: 1, tag: '24H', selected: false, created: false },
-    { tagId: 2, tag: 'Sale', selected: false, created: false },
-    { tagId: 3, tag: 'Discount', selected: false, created: false }
-  ];
-
+export class TagHelperComponent implements OnInit {
+  tags: ProductTag[] = [];
   showCreateNewTagForm: boolean = false;
   newTag: string = '';
+
+  constructor(private tagsService: TagsService) { }
+
+  ngOnInit() {
+    this.tagsService.getAvailableTags().subscribe((tags) => {
+      this.tags = tags;
+    });
+  }
 
   createNewTag() {
     if (this.newTag.trim() !== '') {
       const newTagId = this.tags.length + 1;
       const newTag: ProductTag = { tagId: newTagId, tag: this.newTag, selected: false, created: true };
-      this.tags.push(newTag);
+
+      this.tagsService.addNewTag(newTag);
       this.newTag = '';
     }
   }
 
   check(tag: ProductTag) {
     tag.selected = true;
+    this.tagsService.updateTag(tag);
   }
 
   unCheck(tag: ProductTag) {
     tag.selected = false;
+    this.tagsService.updateTag(tag);
   }
 
-  deleteTag(index: number) {
-    this.tags.splice(index, 1);
+  deleteTag(tag: ProductTag) {
+    const index = this.tags.indexOf(tag);
+    if (index !== -1) {
+      const tagId = tag.tagId as number;
+      this.tags.splice(index, 1);
+      this.tagsService.removeTag(tagId);
+    }
   }
+
 }
--- QAClient/src/app/services/tags.service.ts
+++ QAClient/src/app/services/tags.service.ts
@@ -1,15 +1,44 @@
 import { Injectable } from '@angular/core';
 import { ProductTag } from '../models/productTag';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TagsService {
-  availableTags: ProductTag[] = [
+  private tagsSubject = new BehaviorSubject<ProductTag[]>([
     { tagId: 1, tag: '24H', selected: false },
     { tagId: 2, tag: 'Sale', selected: false },
     { tagId: 3, tag: 'Discount', selected: false }
-  ];
+  ]);
+  tags$ = this.tagsSubject.asObservable();
 
   constructor() { }
+
+  getAvailableTags(): Observable<ProductTag[]> {
+    return this.tags$;
+  }
+
+  getAvailableTagById(tagId: number): ProductTag | undefined {
+    return this.tagsSubject.value.find((tag: ProductTag) => tag.tagId === tagId);
+  }
+
+  addNewTag(newTag: ProductTag): void {
+    this.tagsSubject.next([...this.tagsSubject.value, newTag]);
+  }
+
+  updateTag(updatedTag: ProductTag): void {
+    const updatedTags = this.tagsSubject.value.map((tag: ProductTag) => {
+      if (tag.tagId === updatedTag.tagId) {
+        return { ...tag, selected: updatedTag.selected };
+      }
+      return tag;
+    });
+    this.tagsSubject.next(updatedTags);
+  }
+
+  removeTag(tagId: number): void {
+    const updatedTags = this.tagsSubject.value.filter((tag: ProductTag) => tag.tagId !== tagId);
+    this.tagsSubject.next(updatedTags);
+  }
 }
