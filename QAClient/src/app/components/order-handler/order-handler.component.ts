2e839f3315cccd5ccf300a989e17b7de7d9bfa1f
Lori <lorikmanaj@hotmail.com>
2023-11-09 15:08:32 +0100

    Added order paginator and sort

--- QAClient/src/app/components/order-handler/order-handler.component.ts
+++ QAClient/src/app/components/order-handler/order-handler.component.ts
@@ -1,54 +1,78 @@
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Order } from 'src/app/models/order';
 import { OrderService } from 'src/app/services/order.service';
-import { MatSort, Sort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
-import { LiveAnnouncer } from '@angular/cdk/a11y';
 
 @Component({
   selector: 'app-order-handler',
   templateUrl: './order-handler.component.html',
   styleUrls: ['./order-handler.component.css'],
 })
-export class OrderHandlerComponent implements AfterViewInit {
-  orders: MatTableDataSource<Order> = new MatTableDataSource<Order>();
-  displayedColumns: string[] = ['orderId', 'client', 'date', 'status', 'shippingAddress', 'payment', 'notes', 'total', 'discounts'];
+export class OrderHandlerComponent implements OnInit {
+  orders: Order[] = [];
+  currentPage: number = 1;
+  pageSize: number = 10;
+  maxPage: number = 1; // Initialize maxPage to 1 initially
+  selectedSortOption: 'latest' | 'newest' = 'latest'; // Initialize with 'latest'
 
 
-  pageSize = 5; // Initial page size
-  pageSizeOptions: number[] = [5, 10, 25]; // Options for page size selection
+  constructor(private orderService: OrderService) { }
 
-
-  @ViewChild(MatSort) sort!: MatSort;
-
-  constructor(private orderService: OrderService,
-    private _liveAnnouncer: LiveAnnouncer) { }
-
-  ngAfterViewInit() {
-    this.orders.sort = this.sort;
+  ngOnInit(): void {
+    this.loadOrders();
   }
 
-  announceSortChange(sortState: Sort) {
-    // This example uses English messages. If your application supports
-    // multiple language, you would internationalize these strings.
-    // Furthermore, you can customize the message to add additional
-    // details about the values being sorted.
-    if (sortState.direction) {
-      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
-    } else {
-      this._liveAnnouncer.announce('Sorting cleared');
+  loadOrders() {
+    // Assuming you get the orders from the API response
+    this.orderService.getOrdersPage(this.currentPage, this.pageSize)
+      .subscribe((orders) => {
+        this.orders = orders;
+        this.maxPage = Math.ceil(orders.length / this.pageSize);
+  
+        // Sort the orders by latest after loading
+        this.sortOrdersByLatest();
+      });
+  }
+  
+  loadPreviousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.loadOrders();
     }
   }
 
-  ngOnInit() {
-    this.orderService.getOrders().subscribe(
-      (orders: Order[]) => {
-        this.orders.data = orders;
-        console.log('Orders:', this.orders.data);
-      },
-      (error) => {
-        console.error('Error fetching orders:', error);
+  loadNextPage() {
+    this.currentPage++;
+    this.loadOrders();
+  }
+  // button Next Disabled if pagesize  small than 10 or current page is bigger maxpage
+  // isNextButtonDisabled(): boolean {
+  //   return this.pageSize <= 10 || this.currentPage > this.maxPage;
+  // }
+
+  sortOrdersByLatest() {
+    this.orders.sort((a, b) => {
+      if (this.selectedSortOption === 'latest') {
+        return b.date.getTime() - a.date.getTime(); // Sorting in descending order
       }
-    );
+      return 0; // Default to no sorting
+    });
+  }
+  
+  sortOrdersByNewest() {
+    this.orders.sort((a, b) => {
+      if (this.selectedSortOption === 'newest') {
+        return a.date.getTime() - b.date.getTime(); // Sorting in ascending order
+      }
+      return 0; // Default to no sorting
+    });
+  }
+  
+  
+  onSortOptionChange() {
+    if (this.selectedSortOption === 'latest') {
+      this.sortOrdersByLatest();
+    } else if (this.selectedSortOption === 'newest') {
+      this.sortOrdersByNewest();
+    }
   }
 }
